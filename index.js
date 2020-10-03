b6869c4b7ecc836e0cc5fd315dbbf577807feeb7
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-10-03 11:13:30 -0300

    Preparing for deployment

--- index.js
+++ index.js
@@ -96,5 +96,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
-app.listen(PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
